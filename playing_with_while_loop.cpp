8d80386fe716ff0c176a51ce80aba95d5b6a1315
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-01 14:44:40 +0530

    code for this pattern * ** *** **** *****

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -98,3 +98,28 @@ int main(){
         i=i+1;
     }
 }
+
+
+/* code for this pattern 
+*
+**
+***
+****
+*****
+
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        while(j <= i){
+            cout<<"*";
+            j=j+1;
+        }cout<<endl;
+        i=i+1;
+    }
+}
