99631317f6333a52181582a25628b79cb8d81a5e
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-01 14:36:08 +0530

    code to find 111                222                333

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -1,3 +1,4 @@
+//code to find sum of all even number 
 #include<iostream>
 using namespace std;
 int main(){
@@ -16,7 +17,7 @@ int main(){
 }
 
 
-
+//code to find star pattern in 2x2 form 
 #include<iostream>
 using namespace std;
 int main(){
@@ -27,6 +28,26 @@ int main(){
         int j = 1;
         while(j <= n){
             cout<<"*";
+            j=j+1;
+        }cout<<endl;
+        i=i+1;
+    }
+}
+
+
+/*code to find 111 
+               222
+               333 */
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        while(j <= n){
+            cout<<i;
             j=j+1;
         }cout<<endl;
         i=i+1;
