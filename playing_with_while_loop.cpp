e231053c22b40fd7aba6ea62ae19d9f13ea325e7
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-02 21:46:47 +0530

    /*code for 1            2 3            4 5 6*/

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -123,3 +123,28 @@ int main(){
         i=i+1;
     }
 }
+
+
+
+/*code for 1
+           2 3 
+           4 5 6*/
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout << "enter the number :";
+    cin>>n;
+    int count=0;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            count=count+1;
+            cout<<count<<" ";
+            j=j+1;
+
+        }cout<<endl;
+        i=i+1;
+    }
+}
