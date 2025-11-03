91f075b7239aa4c8e52f8aff10ebe768a416badc
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-03 18:17:45 +0530

    ABC BCD CDE

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -275,3 +275,27 @@ int main (){
         i=i+1;
     }return 0;
 }
+
+
+/*ABC
+BCD
+CDE*/
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout << "enter the number :";
+    cin>>n;
+    char ch = 'A';
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            ch='A'+i+j-2;
+            cout<<ch ;
+            j=j+1;
+
+        }cout<<endl;
+        i=i+1;
+    }return 0;
+}
