a476d30a709dd94b2527dac9de6962739d354ffb
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-03 18:08:13 +0530

    ABC DEF GHI

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -250,3 +250,28 @@ int main (){
         i=i+1;
     }return 0;
 }
+
+
+
+/*ABC
+DEF
+GHI*/
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout << "enter the number :";
+    cin>>n;
+    char ch = 'A';
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<ch ;
+            ch = ch + 1;
+            j=j+1;
+
+        }cout<<endl;
+        i=i+1;
+    }return 0;
+}
