0fbdab975a3ee822245c0f785c0563f8b3976e6b
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-03 17:54:00 +0530

    ABCDE ABCDE ABCDE ABCDE ABCDE

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -223,3 +223,30 @@ int main (){
         i=i+1;
     }return 0;
 }
+
+
+// ABCDE
+// ABCDE
+// ABCDE
+// ABCDE
+// ABCDE
+
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout << "enter the number :";
+    cin>>n;
+    
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            char ch = 'A'+j-1;
+            cout<<ch ;
+            j=j+1;
+
+        }cout<<endl;
+        i=i+1;
+    }return 0;
+}
