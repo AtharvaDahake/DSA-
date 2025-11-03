c928cc6efe6a8292aee95a2177058a9d3504a45e
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-03 18:26:39 +0530

    A BB CCC DDDD EEEEE

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -299,3 +299,30 @@ int main (){
         i=i+1;
     }return 0;
 }
+
+
+
+/*A
+BB
+CCC
+DDDD
+EEEEE*/
+#include<iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout << "enter the number :";
+    cin>>n;
+    char ch = 'A';
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            ch='A'+i-1;
+            cout<<ch ;
+            j=j+1;
+
+        }cout<<endl;
+        i=i+1;
+    }return 0;
+}
