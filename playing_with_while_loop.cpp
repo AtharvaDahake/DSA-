8d18b3546f93fa96979d04b92d3299696edf785b
Atharva Dahake <atharvadahake124@gmail.com>
2025-11-04 10:27:35 +0530

    ***** **** *** ** *

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -408,3 +408,27 @@ int main(){
         i=i+1;
     }
 }
+
+
+// *****
+// ****
+// ***
+// **
+// *
+#include<iostream>
+using namespace std ;
+int main(){
+    int n;
+    cout << "enter the number :";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n-i+1){
+            cout<<"*";
+            j=j+1;
+
+        }cout<<endl;
+        i=i+1;
+    }
+}
