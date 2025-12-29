6663a34d368644f48b951098cb358ca28055acc3
AtharvaDahake <131685891+AtharvaDahake@users.noreply.github.com>
2025-12-29 13:35:57 +0530

    Change initial value of i from 0 to 1

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -503,7 +503,7 @@ using namespace std;
 int main() {
     int n ;   
     cin>>n;     
-    int i = 0;        
+    int i = 1;        
     int count = 1;      
     while (i <= n) {
         int spaces = n - i;   
