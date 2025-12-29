cd81b243e1762497dc12e47bc5cd00f0f975c99c
AtharvaDahake <131685891+AtharvaDahake@users.noreply.github.com>
2025-12-29 14:56:28 +0530

    Update playing_with_while_loop.cpp

--- playing_with_while_loop.cpp
+++ playing_with_while_loop.cpp
@@ -554,3 +554,36 @@ int main() {
         i++;
     }
 }
+
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n ;   
+    cin>>n;     
+    int i = 1;            
+    while (i <= n) {
+        int spaces = n - i;   
+        while (spaces) {
+            cout <<" ";
+            spaces=spaces-1;
+        }int j=1;
+        while (j <= i) {
+            cout<<j;
+            j++;
+        }
+        int x = i-1;
+        while(x){
+            cout<<x;
+            x=x-1;
+        }
+        cout << endl;
+        i++;
+    }
+}
