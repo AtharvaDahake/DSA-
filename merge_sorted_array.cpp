7f598a1f529d43c7a7e1b9208a8eccf57bf04f8f
AtharvaDahake <131685891+AtharvaDahake@users.noreply.github.com>
2026-01-05 17:33:20 +0530

    merge function for merging two sorted arrays

--- merge_sorted_array.cpp
+++ merge_sorted_array.cpp
@@ -1,37 +1,49 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-void mergearr(int arr1[],int n, int arr2[],int m,int arr3[]);
-void print(int arr3[],int n);
-int main(){
-    int arr1[4]={1,3,5,7};
-    int arr2[4]={2,4,6,8};
-    int arr3[8]={0};
-    mergearr(arr1,4,arr2,4,arr3);
-    print(arr3,8);
-    return 0;
-}
-void print(int arr3[], int n){
-    for(int i=0;i<n;i++){
-        cout << arr3[i] << " ";
-    }cout << endl;
-}
-void mergearr(int arr1[],int n, int arr2[],int m,int arr3[]){
-    int i=0;
-    int j=0;
+void merge(int arr[],int n,int arr1[],int m,int arr2[]){
+    int i=0,j=0;
     int k=0;
-    while(i<n&&j<m){
-        if(arr1[i]<arr2[j]){
-            arr3[k++]=arr1[i++];
-            
+    while(i<n && j<m){
+        if(arr[i]<arr1[j]){
+            arr2[k++]=arr[i++];
+            // k++;
+            // i++;
+        }else{
+            if(arr[i]>arr1[j]){
+                arr2[k++]=arr1[j++];
+                // k++;
+                // j++;
+            }
         }
-        else{
-            arr3[k++]=arr2[j++];  
-        }
-    }
-    while(i<n){
-        arr3[k++]=arr1[i++];
+    }while(i<n){
+        arr2[k++]=arr[i++];
+        // k++;
+        // i++;
     }
     while(j<m){
-        arr3[k++]=arr2[j++];
+        arr2[k++]=arr[j++];
+        // k++;
+        // j++;
     }
+}
+
+void print(int ans[],int n){
+    for(int i=0;i<n;i++){
+        cout<< ans[i]<<" ";
+    }cout<<endl;
+}
+int main(){
+    int arr[5]={1,3,5,7,9};
+    int arr1[4]={2,4,6,8};
+    int arr2[8]={0};
+    for(int i=0;i<5;i++){
+        cout<< arr[i]<<" ";
+    }cout<<endl;
+    for(int i=0;i<4;i++){
+        cout<< arr1[i]<<" ";
+    }cout<<endl;
+    merge(arr,5,arr1,4,arr2);
+    print(arr2,8);
+    return 0;
 }
