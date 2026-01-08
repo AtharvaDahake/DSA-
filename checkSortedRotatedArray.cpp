d69419e2f8c6112bdebc806d37b2e9cb0173b01d
AtharvaDahake <131685891+AtharvaDahake@users.noreply.github.com>
2026-01-08 14:49:39 +0530

    Implement function to check sorted rotated array

---
+++ checkSortedRotatedArray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool rotate(vector<int>& nums){
+    int count=0;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        if(nums[i]>nums[i+1]%n){
+            count++;
+        }
+    }
+    return count<=1;
+}
