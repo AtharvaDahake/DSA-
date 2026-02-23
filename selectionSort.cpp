2e1df118f939159e5832efa1b5f1b47d505b2b02
AtharvaDahake <131685891+AtharvaDahake@users.noreply.github.com>
2026-02-23 14:30:16 +0530

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,27 +1,42 @@
 #include<iostream>
-using namespace std ;
-void selectsort(int arr[],int n){
+#include<vector>
+using namespace std;
+
+void selectionsort(vector<int> &arr,int n){
+
     for(int i=0;i<n-1;i++){
-        int minindex=i;
+
+        int minindex = i;
+
         for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[minindex]){
-            minindex=j;
+            
+            if(arr[minindex]>arr[j]){
+                minindex=j;
             }
-        }swap(arr[minindex],arr[i]);
+        }
+        swap(arr[minindex],arr[i]);
     }
 }
-int main(){
-    int n;
-    cout << " enter the size of array : ";
-    cin >>n;
-    int arr[100];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }cout << endl;
-    selectsort(arr,n);
-    cout << "Sort array is :";
+
+void print(vector<int> arr,int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }cout<<endl;
+}
+
+int main(){
+
+    vector<int> arr={4,56,72,4,2,234,232,5,34,563,423,34,5456,23,123457,45,634};
+    int size=arr.size();
+
+    cout<<"befor sorting ";
+    print(arr,size);
+    cout<<endl;
+    
+    cout<<"after sorting ";
+    selectionsort(arr,size);
+    print(arr,size);
+    
     return 0;
+
 }
